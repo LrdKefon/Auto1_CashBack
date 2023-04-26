928e7deb7d0c9c55c077b113a9cfbda386492643
Alex <zazerny@gmail.com>
2023-04-26 13:21:25 +0300

    Redid tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,21 +10,21 @@ public class CashbackHackServiceTest {
     @Test
     public void amountBelowBoundary() {
         int actual = service.remain(999);
-        int expected = 0;
+        int expected = 1;
         assertEquals(actual, expected);
     }
 
     @Test
     public void amountAboveBoundary() {
         int actual = service.remain(1001);
-        int expected = 1000;
+        int expected = 999;
         assertEquals(actual, expected);
     }
 
     @Test
     public void amountEqualBoundary() {
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 
